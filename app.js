39cabb116a0abc1132803547d0e2e18ecf9a224a
MStudent1 <mlancaster17@ivytech.edu>
2022-04-11 18:19:06 -0400

    Removed html and replaced with ejs to make web page capable of being dynamic. Added partial templates to reduce potential maintainence on repetitive html tags used on every page.

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const express = require('express');
+const res = require('express/lib/response');
 
-//Express app
+// express app
 const app = express();
 
-//Listen for requests
+// listen for requests
 app.listen(3000);
 
+// register view engine
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.sendFile('/home.html',  { root: __dirname} );
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
-app.get('/About', (req, res) => {
-    res.sendFile('./views/About.html',  { root: __dirname} );
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
 });
 
-//redirect; old relative path, will redirect to new path (/about)
-app.get('/about-us', (req, res) => {
-    res.redirect('/About');
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
 });
 
-//404 page
+// 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+  res.status(404).render('404', { title: '404' });
 });
