f391838513d57e3ae2c69b147a5ad06fba6ce80d
MStudent1 <mlancaster17@ivytech.edu>
2022-04-11 01:00:31 -0400

    Added Express package, created app.js to run erver

--- app.js
+++ app.js
@@ -7,10 +7,19 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html',  { root: __dirname} );
+    res.sendFile('/home.html',  { root: __dirname} );
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html',  { root: __dirname} );
+app.get('/About', (req, res) => {
+    res.sendFile('./views/About.html',  { root: __dirname} );
 });
 
+//redirect; old relative path, will redirect to new path (/about)
+app.get('/about-us', (req, res) => {
+    res.redirect('/About');
+});
+
+//404 page
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', {root: __dirname});
+});
